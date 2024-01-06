75b926ddddd77e4b890fadab6eadccebf7654d27
Aleksandre <aleksandre.nozadze.1@iliauni.edu.ge>
2024-01-06 06:25:43 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
 });
