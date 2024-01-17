128c1bc6357858f78385a53d92fbd5d3b52c0c28
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-01-18 01:04:26 +0400

    Update FAQ.jsx
    dope FAQ added

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -10,27 +10,33 @@ const FAQData = [
 	},
 	{
 		id: 1,
-		question: "How can I become a member of the Iliauni Anime Club?",
+		question: "How can I become a member of the Iliauni Anime Club ?",
 		answer:
 			"Becoming a member of the Iliauni anime club is easy! Simply navigate to our website's 'Get Started' section, where you'll find all the details and applications you need to join us.",
 	},
 	{
 		id: 2,
-		question: "What types of anime-related events and activities does Iliauni Anime Club organize?",
+		question: "What types of anime-related events and activities does Iliauni Anime Club organize ?",
 		answer:
 			"Iliauni anime club hosts a variety of exciting events and activities for anime enthusiasts. From engaging screenings and conventions to themed gatherings, our club offers a diverse range of anime-related experiences!",
 	},
 	{
 		id: 3,
-		question: "Is membership in Iliauni Anime Club limited to students from a specific department or major?",
+		question: "Is membership in Iliauni Anime Club limited to students from a specific department or major ?",
 		answer:
 			"No, membership in the club is open to all students, regardless of their department or major.",
 	},
 	{
 		id: 4,
-		question: "Are there leadership opportunities within Iliauni Anime Club?",
+		question: "Are there leadership opportunities within Iliauni Anime Club ?",
 		answer:
 			"Absolutely! Your contributions within the club won't go unnoticed. We actively encourage members to get involved and stand out through their passion and dedication. Outstanding individuals will be scouted for leadership positions.",
+	},
+	{
+		id: 5,
+		question: "Will you retrun old FAQ ?",
+		answer:
+			"No, we will not retrun old FAQ, due to it being insanely SICK! We can't handle that much heat...",
 	}
 ];
 
