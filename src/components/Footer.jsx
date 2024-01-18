a94afeca7196d555b54e60df6f5c6bdbf7875203
FinnTheHero <aleksandre.nozadze.1@iliauni.edu.ge>
2024-01-18 23:26:21 +0400

    Removed un-used packages
    Fixed footer text as well

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -148,7 +148,7 @@ export const Footer = () => {
 						</div>
 					</div>
 					<p className="lg:text-center text-sm text-gray-400 border-t border-[rgb(255,255,255,0.2)] pt-12 mt-16 hidden lg:block">
-						Website and its logo(except tailcasts logo which is temporary and will be changed) belong to Iliauni Anime Club
+						Website and it's logo belongs to Iliauni Anime Club
 					</p>
 				</div>
 			</div>
